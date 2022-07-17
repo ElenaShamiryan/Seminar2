fa4d7c104ac3c3ad83fad38e2a0acf15f007b635
ElenaShamiryan <elen91@list.ru>
2022-07-17 16:33:05 +0300

    нахождение третьей цифры произвольного числа

---
+++ ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿void ThirdDigitNumber(int number)
+{
+if(number<100) Console.WriteLine("Третьей цифры в данном числе нет");
+if (number>99) 
+{
+int index = 2;
+string s1 = Convert.ToString(number);
+Console.WriteLine($"Третья цифра числа {s1} это {s1[index]}");
+}
+}
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+ThirdDigitNumber(num);
