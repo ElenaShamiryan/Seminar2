af43e3b2c136b65327a8ea6faf8960c11767c38d
ElenaShamiryan <elen91@list.ru>
2022-07-17 16:37:17 +0300

    определение рабочих/выходных дней

---
+++ ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿int DaysOfWeek(int a)
+{
+int result = a;
+if (a <= 5) Console.WriteLine("Этот день является рабочим");
+if (a >= 6 && a<=7) Console.WriteLine("Этот день является выходным");
+if (a>=8) Console.WriteLine("Введите число от 1 до 7");
+return result;
+}
+
+Console.WriteLine("Введите число от 1 до 7 ");
+int number = Convert.ToInt32(Console.ReadLine());
+int days = DaysOfWeek(number);
