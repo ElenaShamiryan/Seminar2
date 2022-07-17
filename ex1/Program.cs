0aa6e79fddb363360f9d5ad6580db1779266d603
ElenaShamiryan <elen91@list.ru>
2022-07-17 16:26:32 +0300

    нахождение второй цифры трехзначного числа

---
+++ ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿int ThreeDigitNumber(int num)
+
+{
+    int n = num / 10;
+    int b = n % 10;
+    int c = b;
+
+    return c;
+}
+
+int number = new Random().Next(100,999);
+int digit = ThreeDigitNumber(number);
+
+Console.WriteLine($"Вторая цифра трехзначного числа {number} это {digit}");
